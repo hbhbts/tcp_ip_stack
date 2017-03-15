9dc5238273fc9f594164f52337fe28a4f57225af
hbhbts <haitao73@qq.com>
2017-03-15 21:47:52 +0800

    update

--- tcp_ip_top.cpp
+++ tcp_ip_top.cpp
@@ -12,7 +12,9 @@ typedef struct axis_word_t {
             :data(data), keep(keep), last(last) {}
 }
 
-const local_mac_addr = 0x0a0b0d01020304;
+const ap_uint<48> local_mac_addr        = 0x0a0b01020304;
+const ap_uint<48> broadcast_mac_addr    = 0xffffffffffff;
+const ap_uint<32> broadcast_ip_addr     = 0xffffffff;
 
 void tcp_ip_top(stream<axis_word_t> rx_data, stream<axis_word_t> tx_data) {
 #pragma HLS interface ap_ctrl_none port=return
@@ -41,7 +43,7 @@ void rx_mac_decode(stream<axis_word_t> rx_data, stream<axis_word_t> rx_arp_data,
 #pragma HLS inline off
 #pragma HLS pipeline II=1 enable_flush
 
-    static enum rmd_state_e {RMD_R0 = 0, RMD_R1, RMD_R2, RMD_R3, RMD_R4, RMD_STREAM, RMD_RESIDUE} rmd_state = RMD_IDLE;
+    static enum rmd_state_e {RMD_IDLE = 0, RMD_R1, RMD_R2, RMD_R3, RMD_R4, RMD_STREAM, RMD_RESIDUE} rmd_state = 0;
     static axis_word_t curr_word;
     static axis_word_t perv_word;
     static axis_word_t out_word = axis_word_t(0, 0xf, 0);
@@ -53,42 +55,46 @@ void rx_mac_decode(stream<axis_word_t> rx_data, stream<axis_word_t> rx_arp_data,
                 dst_mac_addr(47, 16) = byte_swap32(rx_data.data);
                 rmd_state ++;
             }
+            break;
         case RMD_R1:
             if(!rx_data.empty()) {
                 curr_word = rx_data.read();
-                dst_mac_addr(15, 0) = rx_data.data(15, 0);
+                dst_mac_addr(15, 0) = byte_swap16(rx_data.data(15, 0));
                 rmd_state ++;
             }
+            break;
         case RMD_R2:
             if(!rx_data.empty()) {
                 curr_word = rx_data.read();
                 rmd_state ++;
             }
+            break;
         case RMD_R3:
             if(!rx_data.empty()) {
                 curr_word = rx_data.read();
                 prev_word = curr_word;
-                eth_type = rx_data.data(15, 0);
+                eth_type = byte_swap16(curr_word.data(15, 0));
                 rmd_state ++;
             }
+            break;
         case RMD_R4:
             if(!rx_data.empty()) {
                 curr_word = rx_data.read();
-                prev_word = curr_word;
                 out_word.data(15, 0) = prev_word.data(31, 16);
                 out_word.data(31, 16) = curr_word.data(15, 0);
-                if(dst_mac_addr == local_mac_addr || dst_mac_addr == 0xffffffffffff) {
+                if(dst_mac_addr == local_mac_addr || dst_mac_addr == broadcast_mac_addr) {
                     if(eth_type == ETH_TYPE_ARP)
                         rx_arp_data.write(out_word);
                     else if(eth_type == ETH_TYPE_IP)
                         rx_ip_data.write(out_word);
                 }
+                prev_word = curr_word;
                 rmd_state ++;
             }
+            break;
         case RMD_STREAM:
             if(!rx_data.empty()) {
                 curr_word = rx_data.read();
-                prev_word = curr_word;
                 out_word.data(15, 0) = prev_word.data(31, 16);
                 out_word.data(31, 16) = curr_word.data(15, 0);
                 if(keep_count(curr_word.keep) < 3 && curr_word.last == 1) {
@@ -102,7 +108,9 @@ void rx_mac_decode(stream<axis_word_t> rx_data, stream<axis_word_t> rx_arp_data,
                     rx_arp_data.write(out_word);
                 else if(eth_type == ETH_TYPE_IP)
                     rx_ip_data.write(out_word);
+                prev_word = curr_word;
             }
+            break;
         case RMD_RESIDUE:
             out_word.data(15, 0) = prev_word.data(31, 16);
             out_word.data(31, 16) = 0;
@@ -114,6 +122,7 @@ void rx_mac_decode(stream<axis_word_t> rx_data, stream<axis_word_t> rx_arp_data,
                 rx_arp_data.write(out_word);
             else if(eth_type == ETH_TYPE_IP) 
                 rx_ip_data.write(out_word);
+            break;
     }
 }
 
@@ -123,6 +132,19 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
 #pragma HLS inline off
 #pragma HLS interface ap_stable port=dchp_ip_addr
 #pragma HLS pipeline II=1 enable_flush
+    static enum rid_state_e {RID_IDLE = 0, RID_R1, RID_R2, RID_R3, RID_R4, RID_SKIP, RID_STREAM, RID_KEEP, RID_RESIDUE} rid_state = 0;
+    static ap_uint<4> ihl;
+    static ap_uint<4> version;
+    static ap_uint<16> total_length;
+    static ap_uint<17> calc_checksum;
+    static ap_uint<3> flags;
+    static ap_uint<13> offset;
+    static ap_uint<8> ttl;
+    static ap_uint<8> protocol;
+    static ap_uint<16> ip_checksum;
+    static ap_uint<32> dst_ip_addr;
+    static axis_word_t curr_word;
+    static axis_word_t prev_word;
 
     switch(rid_state) {
         case RID_IDLE:
@@ -136,16 +158,19 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
                 ihl --;
                 rid_state ++;
             }
+            break;
         case RID_R1:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
-                flags = curr_word.data(18, 16);
-                offset = curr_word.data(31, 19);
+                flags = curr_word.data(23, 21);
+                offset(12, 8) = curr_word.data(20, 16);
+                offset(7, 0) = curr_word.data(31, 24);
                 calc_checksum = curr_word.data(31, 16) + curr_word.data(15, 0);
                 calc_checksum = calc_checksum(15, 0) + calc_checksum.bit(16);
                 ihl --;
                 rid_state ++;
             }
+            break;
         case RID_R2:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
@@ -157,19 +182,22 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
                 ihl --;
                 rid_state ++;
             }
+            break;
         case RID_R3:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
-                src_ip_addr = curr_word.data;
+                rx_udp_data.write(curr_word.data);
                 calc_checksum = curr_word.data(31, 16) + curr_word.data(15, 0);
                 calc_checksum = calc_checksum(15, 0) + calc_checksum.bit(16);
                 ihl --;
-                rid_state ++
+                rid_state ++;
             }
+            break;
         case RID_R4:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
-                dst_ip_addr = curr_word.data;
+                dst_ip_addr = byte_swap32(curr_word.data);
+                rx_udp_data.write(curr_word.data);
                 calc_checksum = curr_word.data(31, 16) + curr_word.data(15, 0);
                 calc_checksum = calc_checksum(15, 0) + calc_checksum.bit(16);
                 ihl --;
@@ -180,6 +208,7 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
                 else 
                     rid_state = RID_SKIP;
             }
+            break;
         case RID_SKIP:
             if(!rx_ip_data.empty()) {
                 rx_ip_data.read();
@@ -192,28 +221,38 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
                     rid_state = RID_STREAM;
                 }
             }
+            break;
         case RID_STREAM:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
-                no_drop = version == 4 && !flags && !offset && dst_ip_addr == dchp_ip_addr && calc_checksum == 0;
+                axis_word_t temp = axis_word_t(0, 0xf, 0);
+                temp.data(15, 8) = protocol;
+                temp.data(15, 8) = total_length(7, 0);
+                temp.data(7, 0) = total_length(15, 8);
+                rx_udp_data.write(temp);
+                no_drop = version == 4 && !flags && !offset && (dst_ip_addr == dchp_ip_addr || dst_ip_addr == broadcast_ip_addr) && calc_checksum == 0;
                 if(!no_drop)
                     rid_state = RID_DROP;
-                else if(total_length < 4)
+                else if(total_length < 5)
                     rid_state = RID_RESIDUE;
                 else 
                     rid_state = RID_KEEP;
                 prev_word = curr_word;
+                prev_word.last = 0;
             }
+            break;
         case RID_DROP:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
                 if(curr_word.last)
                     rid_state = RID_IDLE;
             }
+            break;
         case RID_KEEP:
             if(!rx_ip_data.empty()) {
                 curr_word = rx_ip_data.read();
-                if(curr_word.last)
+                total_length = total_length - 4;
+                if(total_length < 5)
                     rid_state = RID_RESIDUE;
                 if(protocol == IP_TYPE_ICMP)
                     rx_icmp_data.write(prev_word);
@@ -221,15 +260,13 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
                     rx_expired_data.write(prev_word);
                 else if(protocol == IP_TYPE_UDP)
                     rx_udp_data.write(prev_word);
-                total_length = total_length - 4;
                 prev_word = curr_word;
-
+                prev_word.last = 0;
             }
+            break;
         case RID_RESIDUE:
-                if(total_length < 4) {
-                    prev_word.keep = ((1 << total_length) - 1) + (1 << total_length);
-                    prev_word.last = 1;
-                }
+                prev_word.keep = ~(1 << (total_length - 1)) + (1 << (total_length - 1));
+                prev_word.last = 1;
                 if(protocol == IP_TYPE_ICMP)
                     rx_icmp_data.write(prev_word);
                 else if(ttl == 1)
@@ -238,6 +275,13 @@ void rx_ip_decode(stream<axis_word_t> rx_ip_data, stream<axis_word_t> rx_icmp_da
                     rx_udp_data.write(prev_word);
                 rid_state = RID_IDLE;
             }
-
+            break;
     }
+}
+
+arp_server(rx_arp_data, ip_query, ip_query_resp, tx_arp_data, dchp_ip_addr);
+void arp_server(stream<axis_word_t > rx_arp_data, stream<ap_uint<32> > ip_query, stream<query_resp_t> ip_query_resp, stream<axis_word_t> tx_arp_data, ap_uint<32> dchp_ip_addr) {
+#pragma HLS inline off
+#pragma HLS pipeline II=1 enable_flush
+
 }
